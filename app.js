7613f4712f4f1d6cee0eb7c24ffa98421b9dbb0c
David Claivaz <david.claivaz@gmail.com>
2026-05-09 21:34:27 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ fetch('questions.json')
     const answers = {};
 
 
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+    
 
 function updateProgress(answerCount, total) {
   const percent = total === 0
